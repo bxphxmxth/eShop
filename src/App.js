98a6e28540d95d605cada82d68fc63fa1c1cc12e
bxphxmxth <fushs@hotmail.fr>
2021-06-28 13:50:41 +0200

    fctAj

--- src/App.js
+++ src/App.js
@@ -3,15 +3,14 @@ import './App.css';
 import React from "react";
 import Articles from "./components/Articles"
 
-let articles = ["pinceau", "toile", "fusain"];
-let panier = [];
+let argent = 20;
 
 
 class App extends React.Component {
   
-  state = {
-    argent : "20"
-  }
+  // state = {
+  //   argent : "20"
+  // }
 
 
 
--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,15 +1,27 @@
 import React from "react";
+import App from "../App";
+let panier = [];
+let articles = ["pinceau", "toile", "fusain"];
+
 
 class Articles extends React.Component{
 
+     ajouterPanier = (e) =>{
+          //  Push objet dans panier
+          e.target = panier.push(e.target.parentElement.innerText)
+          console.log(panier)
+          // split tableau objet 
+          //if objet est pinceau retirer 5 euros ...
+          if()
+     }
 
      render(){
           return(
                <div>
           <ul>
-               <li>Pinceau</li>
-               <li>Toile</li>
-               <li>Fusain</li>
+               <li>Pinceau <button onClick={this.ajouterPanier}>acheter</button></li>
+               <li>Toile <button onClick={this.ajouterPanier}>acheter</button></li>
+               <li>Fusain <button onClick={this.ajouterPanier} >acheter</button></li>
           </ul>
      </div>
           
